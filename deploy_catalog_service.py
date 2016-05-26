179d59d5618ee51f74bec1460ca5cc125eddec79
Michael McEniry [UAH] <mcenirm@uah.edu>
2016-05-26 15:54:51 -0500

    Add function steps to a loop

--- deploy_catalog_service.py
+++ deploy_catalog_service.py
@@ -26,36 +26,39 @@ def main():
         '\0'
      )
 
-    function_metadata = {
-        'label': 'catalog_service',
-        'description': 'Looking up service information.',
-    }
-    function_label = function_metadata['label']
-    function_description = function_metadata['description']
+    for function_metadata in [
+        {
+            'label': 'catalog_service',
+            'description': 'Looking up service information.',
+        },
+    ]:
+        function_label = function_metadata['label']
+        function_description = function_metadata['description']
 
-    function_module = function_label
-    function_module_file = function_module + '.py'
-    function_package_file = str(build / (function_label + '.zip'))
+        function_module = function_label
+        function_module_file = function_module + '.py'
+        function_package_file = str(build / (function_label + '.zip'))
 
-    util.create_deployment_package(
-        function_package_file,
-        [function_module_file]
-    )
-    function_arn = util.create_or_update_lambda_function(
-        function_package_file,
-        function_label,
-        "arn:aws:iam::"+ACCOUNT_NUMBER+":role/lambda_s3",
-        function_module + '.lambda_handler',
-        function_description,
-        ACCOUNT_NUMBER
-    )
+        util.create_deployment_package(
+            function_package_file,
+            [function_module_file]
+        )
+        function_arn = util.create_or_update_lambda_function(
+            function_package_file,
+            function_label,
+            "arn:aws:iam::"+ACCOUNT_NUMBER+":role/lambda_s3",
+            function_module + '.lambda_handler',
+            function_description,
+            ACCOUNT_NUMBER
+        )
 
-    util.replace_instances_in_file(
-        catalog_service_swagger_file,
-        catalog_service_swagger_file,
-        '$' + function_metadata['label'] + 'ARN$',
-        function_arn
-    )
+        util.replace_instances_in_file(
+            catalog_service_swagger_file,
+            catalog_service_swagger_file,
+            '$' + function_metadata['label'] + 'ARN$',
+            function_arn
+        )
+
     api_id = util.create_or_update_api(
         catalog_service_swagger_file
     )
