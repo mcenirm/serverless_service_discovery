235dcd03c5be19afd43e500f353a70af2d277288
Michael McEniry [UAH] <mcenirm@uah.edu>
2016-05-26 19:59:15 -0500

    Also undeploy register/deregister functions

--- undeploy_catalog_service.py
+++ undeploy_catalog_service.py
@@ -2,9 +2,15 @@ def main():
     import util
 
     name = 'catalog_service'
+    function_names = [
+        name,
+        'catalog_register',
+        'catalog_deregister',
+    ]
 
     util.delete_rest_api_by_name(name)
-    util.delete_function(name)
+    for function_name in function_names:
+        util.delete_function(function_name)
 
 
 if __name__ == '__main__':
